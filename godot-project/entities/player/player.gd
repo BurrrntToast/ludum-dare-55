151a5265c869037c1ad3c40c1da1f1320698acd1
BurrrntToast <146839305+BurrrntToast@users.noreply.github.com>
2024-04-13 13:11:30 +0100

    basic implmentation of 'summoning'
    - will need major changing but this will work for now
    
    #2

--- godot-project/entities/player/player.gd
+++ godot-project/entities/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const STATIC_PLAYER = preload("res://entities/static player/static_player.tscn")
+
 @export var speed : float = 150.0
 
 @export var jump_height : float = 20.0
@@ -9,6 +11,14 @@ extends CharacterBody2D
 @onready var jump_velocity : float = ((2.0 * jump_height) / time_to_peak_time) * -1.0
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (time_to_peak_time * time_to_peak_time)) * -1.0
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (time_to_descent_time * time_to_descent_time)) * -1.0
+
+func _process(delta):
+	
+	if Input.is_action_just_pressed("summon"):
+		var STATIC_PLAYER_INSTANCE = STATIC_PLAYER.instantiate()
+		STATIC_PLAYER_INSTANCE.global_position = global_position
+		GlobalScript.current_shell_holder.add_child(STATIC_PLAYER_INSTANCE)
+		global_position.y = -20
 
 func _physics_process(delta):
 	velocity.y += get_gravity() * delta # apply gravity
