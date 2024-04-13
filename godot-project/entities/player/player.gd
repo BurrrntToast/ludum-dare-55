feb0568d764643732974a682c9a2c327ea793227
BurrrntToast <146839305+BurrrntToast@users.noreply.github.com>
2024-04-13 16:13:40 +0100

    stopped player moving while spawning
     + some other bits
    
    #2

--- godot-project/entities/player/player.gd
+++ godot-project/entities/player/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 const STATIC_PLAYER = preload("res://entities/static player/static_player.tscn")
 
+@onready var jump_sound = $JumpSound
+@onready var summon_sound = $SummonSound
+
 @export var speed : float = 150.0
 
 @export var jump_height : float = 20.0
@@ -12,24 +15,34 @@ const STATIC_PLAYER = preload("res://entities/static player/static_player.tscn")
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (time_to_peak_time * time_to_peak_time)) * -1.0
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (time_to_descent_time * time_to_descent_time)) * -1.0
 
+var is_active : bool = false
+
 func _process(delta):
+	
+	print(is_active)
+	
+	if is_on_floor() and !is_active:
+		is_active = true
 	
 	if Input.is_action_just_pressed("summon"):
 		var STATIC_PLAYER_INSTANCE = STATIC_PLAYER.instantiate()
 		STATIC_PLAYER_INSTANCE.global_position = global_position
 		GlobalScript.current_shell_holder.add_child(STATIC_PLAYER_INSTANCE)
 		global_position.y = -20
-		$SummonSound.play()
+		is_active = false
+		summon_sound.play()
 
 func _physics_process(delta):
 	velocity.y += get_gravity() * delta # apply gravity
-	handle_movement()
+	
+	if is_active:
+		handle_movement_input()
+
 	move_and_slide()
 
-func handle_movement():
+func handle_movement_input():
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		
 		jump()
 		
 
@@ -41,7 +54,7 @@ func handle_movement():
 	
 func jump():
 	velocity.y = jump_velocity
-	$JumpSound.play()
+	jump_sound.play()
 	
 func get_gravity():
 	if velocity.y < 0.0: return jump_gravity
