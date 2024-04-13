feba3bd1cae6a87908d3baf9c4094214c328ecc3
BurrrntToast <146839305+BurrrntToast@users.noreply.github.com>
2024-04-13 19:40:38 +0100

    summon system + random guy spawns
    - finished summon system
    - random guy spawns
    
    #2

--- godot-project/scenes/dev stuff/dev_level.gd
+++ godot-project/scenes/dev stuff/dev_level.gd
@@ -3,7 +3,7 @@ extends Node2D
 const GUY_1 = preload("res://entities/guys/guy1.tscn")
 const GUY_2 = preload("res://entities/guys/guy2.tscn")
 const GUY_3 = preload("res://entities/guys/guy3.tscn")
-const STATIC_PLAYER = preload("res://entities/static player/static_player.tscn")
+const STATIC_GUY = preload("res://entities/static player/static_player.tscn")
 
 @onready var array_of_guys = [GUY_1, GUY_2, GUY_3]
 
@@ -20,12 +20,21 @@ func _ready():
 	var default_guy = GUY_2.instantiate()
 	default_guy.global_position = guy_spawn_pos.global_position
 	player_holder.add_child(default_guy)
+	current_guy = default_guy
 	
-#func make_static_guy():
-	#var static_guy_instance = STATIC_PLAYER.instantiate()
-	#static_guy_instance.global_position = global_position
-	#GlobalScript.current_shell_holder.add_child(static_guy_instance)
-	#static_guy_instance.setup(guy_id - 1, current_colour)
+func _process(_delta):
+	if Input.is_action_just_pressed("summon"):
+		make_static_guy()
+		summon_random_guy()
+	
+func make_static_guy():
+	var new_static_guy = STATIC_GUY.instantiate()
+	new_static_guy.global_position = current_guy.global_position
+	shell_holder.add_child(new_static_guy)
+	new_static_guy.setup(current_guy.guy_id - 1, current_guy.current_colour)
+
+	current_guy.queue_free()
+	current_guy = null
 
 func summon_random_guy():
 	var new_guy_scene = array_of_guys.pick_random()
@@ -33,6 +42,7 @@ func summon_random_guy():
 	new_guy.global_position = guy_spawn_pos.global_position
 	player_holder.add_child(new_guy)
 	current_guy = new_guy
+	
 #
 #func player_summoned():
 	#velocity = Vector2(0, 0)
