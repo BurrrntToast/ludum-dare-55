7ac9b262894c83cacf225cbe1750e86ecfb9fe86
BurrrntToast <146839305+BurrrntToast@users.noreply.github.com>
2024-05-02 17:26:22 +0100

    made sound sliders work

--- godot-project/menus/options menu/options_menu.gd
+++ godot-project/menus/options menu/options_menu.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 const main_menu : String = "res://menus/main menu/main_menu.tscn"
 
-@onready var music_slider = $Control/CenterContainer/VBoxContainer/Music/Slider
-@onready var sfx_slider = $Control/CenterContainer/VBoxContainer/SFX/Slider
+@onready var music_slider = $Control/CenterContainer/VBoxContainer/Music/MusicSlider
+@onready var sfx_slider = $Control/CenterContainer/VBoxContainer/Sfx/SfxSlider
 @onready var back_button = $Control/CenterContainer/VBoxContainer/BackButton
+
+@onready var music_bus_id = AudioServer.get_bus_index("Music")
+@onready var sfx_bus_id = AudioServer.get_bus_index("SFX")
 
 func _ready():
 	TransitionManager.transition_in()
@@ -12,3 +15,9 @@ func _ready():
 
 func _on_back_button_pressed():
 	TransitionManager.transition_to_scene(main_menu)
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(music_bus_id, linear_to_db(value))
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(sfx_bus_id, linear_to_db(value))
